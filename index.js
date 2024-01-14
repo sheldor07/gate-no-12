c8b0452336467b0c3dbc5232e675cb3cb2e3a664
Yajat Gulati <50955708+sheldor07@users.noreply.github.com>
2024-01-15 00:43:39 +0800

    feat: final version on flight with matching

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const puppeteer = require("puppeteer-core");
+
 const questions = ["Do you believe in life on other planets?",
 "Would you travel back in time if given a chance?",
 "Do you think dreams can predict the future?",
 "Would you choose to live forever if you could?",
-"Do you believe in the existence of a parallel universe?",
-"Would you take a trip to Mars if it was safe and affordable?",
-"Do you think every person has a soulmate?",
-"Would you want to know the exact date and time of your future significant life events?",
-"Do you believe that some people have supernatural abilities?",
-"Would you switch lives with someone for a day if you could?"]
+"Do you believe in the existence of a parallel universe?"]
 const client = new Client({
   puppeteer: {
     args: ["--no-sandbox"],
@@ -47,7 +43,7 @@ function addUserToData(data, userID, name) {
       data = {};
     }
     const 
-    data[userID] = [name, {"yes_no" : [], "other_interests" : ""}];
+    data[userID] = [name, {"yes_no" : [], "other_interests" : "", "flight_no" : "", "is_chatting" : false, "matching_users":[], "bio_entered" : ""}];
     fs.writeFile("data.json", JSON.stringify(data), (err) => {
       if (err) throw err;
       console.log("Data written to file");
@@ -65,15 +61,108 @@ function updateUserData(data, userID, userResponse) {
     });
     return "User Data updated successfully!";
 }
+
+function updateUserDataFlight(data, userID, userResponse) {
+    let userData = data[userID]
+    userData[1]["flight_no"] = userResponse
+    data[userID] = userData
+    fs.writeFile("data.json", JSON.stringify(data), (err) => {
+      if (err) throw err;
+      console.log("Data written to file");
+    });
+    return "User Flight Data updated successfully!";
+}
+function getScore(user_1_answers, user_2_answers){
+  let score = 0;
+  for(let i = 0; i< user_1_answers.length;i=i+1){
+    score += user_1_answers[i]*user_2_answers[i]
+  }
+  return score/user_1_answers.length;
+}
+
+function matchingAlgo(data, available_users, newUserID){
+  //TBD use sophistcated algo to figure out threshold
+  const threshholdValue = 0.2
+  // base case 1 person, rno change
+  let keys = Object.keys(available_users);
+  if(keys > 1){
+    for(let uniqueID of keys){
+      if(uniqueID != newUserID){
+        if(getScore(available_users[uniqueID][1]["yes_no"], available_users[newUserID][1]["yes_no"]) >= threshholdValue){
+          data[newUserID][1]["matching_users"].push(uniqueID)
+          data[uniqueID][1]["matching_users"].push(newUserID)
+          fs.writeFile("data.json", JSON.stringify(data), (err) => {
+            if (err) throw err;
+            console.log("Data written to file");
+          });
+          return "User Flight Data updated successfully!";          
+        }
+      }
+    }
+
+  }
+
+
+}
+
+
+function checkForMatch(newUserID, data, flight_no) {
+  let data = JSON.parse(fs.readFileSync("data.json", "utf8"));
+  let keys = Object.keys(data);
+  let available_users = {}
+  for (let uniqueID of keys) {
+    // TBD: include people that did one conversation but didn't conitnue the match
+    if(!data[uniqueID][1]["is_chatting"] && data[uniqueID][1]["flight_no"] === flight_no) {
+      available_users[uniqueID] = data[uniqueID]
+    }
+  }
+  matchingAlgo(data, available_users, newUserID )
+  // TBD: handle multiple matches (sorry players)
+  // TBD refresh function
+  var data = JSON.parse(fs.readFileSync("data.json", "utf8"));
+
+  if(data[newUserID][1]["matching_users"][0]){
+    // TBD: handling two side consent
+    const matchedUserID = data[newUserID][1]["matching_users"][0]
+    sendMessage(newUserID, "We've got a match! Our intelligent agents are sending your match's bio to you now. Stay tuned!")
+    sendMessage(newUserID,data[data[newUserID][1]["matching_users"][0]][1]["bio_entered"] )
+    
+    sendMessage(matchedUserID, "We've got a match! Our intelligent agents are sending your match's bio to you now. Stay tuned!")
+    sendMessage(matchedUserID,data[newUserID][1]["bio_entered"] )
+    sendMessage(newUserID, "If you'd like to chat with this person, send a message to this very number and we'll connect you both!")
+    
+  }
+
+}
+
+function updateUserDataBio(data, userID, messageBody) {
+  let userData = data[userID]
+  userData[1]["bio_entered"] = messageBody
+  data[userID] = userData
+  fs.writeFile("data.json", JSON.stringify(data), (err) => {
+    if (err) throw err;
+    console.log("Data written to file");
+  });
+  return "User Flight Data updated successfully!";  
+}
+
+function updateUserChattingStatus(data, userID) {
+  let userData = data[userID]
+  userData[1]["is_chatting"] = true
+  data[userID] = userData
+  fs.writeFile("data.json", JSON.stringify(data), (err) => {
+    if (err) throw err;
+    console.log("Data written to file");
+  });
+  return "User Flight Data updated successfully!";    
+}
+
 client.on("message", (message) => {
     try{
         var data = JSON.parse(fs.readFileSync("data.json", "utf8"));
         const userID = message.from;
         const messageBody = message.body;
         const name = message._data.notifyName;
-        const validResponse = [
-            "yes","y","no","n"
-        ]
         if(checkUserExists(userID,data)){
             const numOfQuesAsked = data[userID][1]["yes_no"].length
             if(numOfQuesAsked != questions.length){
@@ -88,19 +177,38 @@ client.on("message", (message) => {
                 }
                 if(numOfQuesAsked === questions.length -1){
                     // TBD : repeat user
-                    sendMessage(userID, "And you're all set!, tell us your flight number and we'll notify you with a match. ")
-
+                    // sendMessage(userID, "And you're all set! Now just enter your flight number to find your matches")
+                    sendMessage(userID, "And you're all set! Please tell us 1 line about yourself (this will be shown to a prospective match before they accept)")
+                } else {
+                    sendMessage(userID, questions[numOfQuesAsked+1])
                 }
-                sendMessage(userID, questions[numOfQuesAsked+1])
 
-            }else{
+            } else if(data[userID][1]["bio_entered"] == "") {
+
+              updateUserDataBio(data, userID, messageBody)
+              sendMessage(userID, "Awesome! Now just enter your flight number to find your matches")
+
+            } else if(data[userID][1]["flight_no"] == ""){
+                // TBD : flight number validation
+                updateUserDataFlight(data, userID, messageBody.toLowerCase())
+                sendMessage(userID, "Great! We'll send you a message when we find you a match!")
+                checkForMatch(data,messageBody)
+            }  else if(!data[userID][1]["is_chatting"] && data[userID][1]["matching_users"].length > 0) {
+                updateUserChattingStatus(data, userID)
+                updateUserChattingStatus(data, data[userID][1]["matching_users"][0])
+                sendMessage(data[uniqueID][1]["matching_users"][0], messageBody)
+            } else if(data[userID][1]["is_chatting"] && data[userID][1]["matching_users"].length > 0) {
+              sendMessage(data[uniqueID][1]["matching_users"][0], messageBody)
             }
 
         }else{
             addUserToData(data,userID,name );
-            sendMessage(userID, "Welcome to Gate No.12, where you're one flight away from changing your life." )
-            sendMessage(userID, "To match you with your future partner, co-founder, mentor or just a new friend, we'll ask you 5 questions." + " You just have to reply with Yes or No. \n Yup, it's that easy." )
+            sendMessage(userID, "Welcome to Gate No.12, where we help you match you with your future partner, co-founder, mentor or just a new friend who's on your flight! To do this, we'll ask you 5 questions. You just have to reply with Yes or No. \n Yup, it's that easy. Your first questions is on its way." )
             sendMessage(userID, questions[0])
         }
+    }
+
+    catch(err){
+      console.log(err)
     }
 })
